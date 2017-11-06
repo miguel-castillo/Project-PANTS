f35ec5ed8b1cd067cd9eec15317fa286a64fb56c
calvinhan13 <chan9@calstatela.edu>
2017-11-05 19:09:21 -0800

    added new document

--- app/src/main/java/com/mec/pants/ProfileActivity.java
+++ app/src/main/java/com/mec/pants/ProfileActivity.java
@@ -4,28 +4,51 @@ import android.content.Intent;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.Button;
 import android.os.Bundle;
 import android.net.Uri;
+import android.widget.Spinner;
+
+import java.lang.reflect.Array;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    CircleImageView circleImageView;
     ImageView imageView;
-    Button button;
+    Button bgbutton;
+    Button profilebutton;
     private static final int PICK_IMAGE = 100;
     Uri imageUri;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        circleImageView = (CircleImageView) findViewById(R.id.circle);
+        profilebutton = (Button) findViewById(R.id.button3);
         imageView = (ImageView) findViewById(R.id.imageView);
-        button = (Button) findViewById(R.id.button2);
+        bgbutton = (Button) findViewById(R.id.button2);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        Spinner mySpinner = (Spinner) findViewById(R.id.spinner1);
+
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(ProfileActivity.this,android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.names));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner.setAdapter(myAdapter);
+
+        profilebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
+        });
+
+        bgbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openGallery();
