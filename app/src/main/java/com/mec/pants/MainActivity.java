c7b705ed7d4958428a8f0ac94ce599fbd046bade
calvinhan13 <chan9@calstatela.edu>
2017-11-09 17:55:07 -0800

    new branch profile crashing

--- app/src/main/java/com/mec/pants/MainActivity.java
+++ app/src/main/java/com/mec/pants/MainActivity.java
@@ -1,27 +1,62 @@
 package com.mec.pants;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private Button btnViewDatabase,btnAddToDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnViewDatabase = (Button) findViewById(R.id.view_items_screen);
 
-        Button inboxButton = (Button) findViewById(R.id.inboxButton);
-        inboxButton.setOnClickListener(new View.OnClickListener() {
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                    toastMessage("Successfully signed in with: " + user.getEmail());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                    toastMessage("Successfully signed out.");
+                }
+                // ...
+            }
+        };
+
+
+        btnAddToDatabase.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, InboxActivity.class);
+                Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
                 startActivity(intent);
-                return;
             }
         });
 
+    }
+    private void toastMessage(String message){
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/mec/pants/ProfileActivity.java
+++ app/src/main/java/com/mec/pants/ProfileActivity.java
@@ -1,29 +1,109 @@
 package com.mec.pants;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+
+import android.app.Activity;
 import android.content.Intent;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Button;
 import android.os.Bundle;
 import android.net.Uri;
+import android.widget.Toast;
 
-public class ProfileActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class ProfileActivity extends Activity {
+
+    private static final String TAG = "AddToDatabase";
 
     ImageView imageView;
-    Button button;
+    private Button button;
+    private Button finish;
+    private Button save;
+    private EditText mAboutme,mEducation,mInterest;
     private static final int PICK_IMAGE = 100;
     Uri imageUri;
+    private String userID;
+
+    private FirebaseDatabase mFirebaseDatabase;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private DatabaseReference myRef;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        mAboutme = (EditText) findViewById(R.id.editText3);
+        mEducation = (EditText) findViewById(R.id.editText4);
+        mInterest = (EditText) findViewById(R.id.editText5);
 
+        save = (Button) findViewById(R.id.button4);
         imageView = (ImageView) findViewById(R.id.imageView);
         button = (Button) findViewById(R.id.button2);
+        finish = (Button) findViewById(R.id.button3);
+
+        mAuth = FirebaseAuth.getInstance();
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        myRef = mFirebaseDatabase.getReference();
+        FirebaseUser user = mAuth.getCurrentUser();
+        userID = user.getUid();
+
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                    toastMessage("Successfully signed in with: " + user.getEmail());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                    toastMessage("Successfully signed out.");
+                }
+                // ...
+            }
+        };
+
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                Log.d(TAG, "onDataChange: Added information to database: \n" +
+                        dataSnapshot.getValue());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+        finish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ProfileActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,6 +111,59 @@ public class ProfileActivity extends AppCompatActivity {
                 openGallery();
             }
         });
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: Submit pressed.");
+                String aboutme = mAboutme.getText().toString();
+                String education = mEducation.getText().toString();
+                String interest = mInterest.getText().toString();
+
+                Log.d(TAG, "onClick: Attempting to submit to database: \n" +
+                        "name: " + aboutme + "\n" +
+                        "email: " + education + "\n" +
+                        "phone number: " + interest + "\n"
+                );
+
+                //handle the exception if the EditText fields are null
+                if(!aboutme.equals("") && !education.equals("") && !interest.equals("")){
+                    UserInformation userInformation = new UserInformation(aboutme,education,interest);
+                    myRef.child("users").child(userID).setValue(userInformation);
+                    toastMessage("New Information has been saved.");
+                    mAboutme.setText("");
+                    mEducation.setText("");
+                    mInterest.setText("");
+                }else{
+                    toastMessage("Fill out all the fields");
+                }
+
+            }
+
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+
+    /**
+     * customizable toast
+     * @param message
+     */
+    private void toastMessage(String message){
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
     }
 
     private void openGallery() {
