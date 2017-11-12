753f61522e26bc901fe2bfef5df718c39c35ff5d
calvinhan13 <chan9@calstatela.edu>
2017-11-11 18:13:31 -0800

    added nav bar

--- app/src/main/java/com/mec/pants/MainActivity.java
+++ app/src/main/java/com/mec/pants/MainActivity.java
@@ -2,9 +2,11 @@ package com.mec.pants;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -12,6 +14,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -44,6 +47,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_profile:
+                        Toast.makeText(MainActivity.this, "Action Add Clicked", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_users:
+                        Toast.makeText(MainActivity.this, "Action user Clicked", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_logout:
+                        Toast.makeText(MainActivity.this, "Action Log out", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
+            }
+        });
         mChatRoomListView = (ListView) findViewById(R.id.addRoomsListView);
         mTextView = (TextView) findViewById(R.id.text);
 
